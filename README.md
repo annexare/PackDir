ab2d82fbc859c5cee1b6e07cc624b5fa3b40216e
Zork <z@z-m13.local>
2016-04-17 22:13:36 +0300

    The library & 7z

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # PackDir
-Pack specified directory with native OS command line tools.
+
+Pack specified directory with native current OS command line tools.
+
+Main purpose was to pack the OS X/Windows Electron app,
+so Linux was not tested yet.
+
+By default, if specified `path` has a `darwin` in it's name,
+the directory will be packed as DMG under OS X.
+
+## Usage
+
+```js
+const Pack = require('pack-dir');
+
+let dir = 'some/test/dir';
+
+// Set custom DMG RegEx, default is `/darwin/`.
+Pack.param('dmg', /osx/);
+
+// Pack the directory
+Pack.path(dir);
+```
+
+### Parameters
+
+* `dmg` `boolean|RegEx`, check to pack into DMG instead of ZIP.
+* `dmgFormat` `string`, the `hdiutil -format` parameter value ([docs](https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man1/hdiutil.1.html)).
+* `silent` `boolean`, will write only Errors to console.
+
+## Credits
+
+Created by [Annexare Studio](https://annexare.com/).
+Feel free to use it as you need in your apps or send updates into [this](https://github.com/annexare/PackDir) public repository.
+All code is under MIT license.
+
+## 7-Zip
+
+For Windows host [7-Zip](http://www.7-zip.org/) standalone console app is used
+(~1MB, doesn't require installation).
+All license files are included with it, with respect to the author (Igor Pavlov).
---
+++ index.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const Path = require('path'),
+    isOSX = (process.platform === 'darwin'),
+    isWindows = (process.platform === 'win32');
+
+let options = {
+    dmg: /darwin/,
+    dmgFormat: 'UDZO',
+    isSilent: false,
+    isSync: true
+};
+
+let setParam = (param, value) => {
+    if (!options.hasOwnProperty(param)) {
+        return false;
+    }
+
+    return options[param] = value
+};
+
+let asDMG = (path) => {
+    if (!isOSX || !options.dmg) {
+        // Impossible to pack as DMG, or disabled
+        return false;
+    }
+    if (options.dmg instanceof RegExp) {
+        return options.dmg.test(path);
+    }
+
+    return !!options.dmg;
+};
+
+let exec = () => {
+    return options.isSync
+        ? require('child_process').execSync
+        : require('child_process').exec;
+};
+
+let get7zPath = () => {
+    return Path.normalize(__dirname + '/7z/7za.exe');
+};
+
+let log = (message) => {
+    if (options.isSilent) {
+        return;
+    }
+
+    console.log(message + "\n");
+};
+
+let packDMG = (path) => {
+    exec()(`hdiutil create -format ${options.dmgFormat} -srcfolder ${path} ${path}.dmg`);
+    log(`DMG file created: "${path}.dmg"`);
+};
+
+let packZIP = (path) => {
+    let pathInfo = Path.parse(path),
+        cmd = isWindows
+            ? `${get7zPath()} a ${pathInfo.base} ${pathInfo.base}.zip`
+            : `zip -r ${pathInfo.base}.zip ${pathInfo.base}`;
+
+    exec()(cmd, { cwd: pathInfo.dir });
+    log(`ZIP archive created: "${path}.zip"`);
+};
+
+let pack = (path) => {
+    if (Array.isArray(path)) {
+        // Recursive packing for Array of paths
+        path.forEach(path => pack(path));
+
+        return true;
+    }
+
+    try {
+        if (asDMG(path)) {
+            packDMG(path);
+        } else {
+            packZIP(path);
+        }
+    }
+    catch (e) {
+        console.error(`Error while packaging "${path}":`, e.message);
+    }
+};
+
+module.exports = {
+    path: pack,
+    param: setParam
+};
